1b6ef072a496527d05a97af796259cd483259cbb
ADBeta <ashleydylanrudd@gmail.com>
2024-01-14 21:09:41 +0000

    Initial Commit

---
+++ include/lib_esp_uart.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* lib_esp_uart is a light and easy to use library for ESP32 / esp-idf devices,
+* it implimets a simple function set to communicate with UART devices and ports
+*
+* This program is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+* FITNESS FOR A PARTICULAR PURPOSE.
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, see www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*
+* ADBeta (c) 2024    Ver 0.0.1    12 Jan 2024
+*******************************************************************************/
+#ifndef LIB_ESP_UART
+#define LIB_ESP_UART
+
+#include "driver/uart.h"
+
+
+
+
+
+#endif
---
+++ lib_esp_uart.c
@@ -0,0 +1,90 @@
+/*******************************************************************************
+* lib_esp_uart is a light and easy to use library for ESP32 / esp-idf devices,
+* it implimets a simple function set to communicate with UART devices and ports
+*
+* This program is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+* FITNESS FOR A PARTICULAR PURPOSE.
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, see www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*
+* ADBeta (c) 2024
+*******************************************************************************/
+#include "lib_esp_uart.h"
+
+#include "driver/uart.h"
+#include <string.h>
+
+/*** Structs ******************************************************************/
+//UART Handler struct used for hardware level control
+typedef struct {
+	uart_port_t port;
+	int tx_io;
+	int rx_io;
+	uint32_t baudrate;
+	int rx_buff;
+	int tx_buff;
+	uint16_t timeout;        //Timeout before giving up reading (milliseconds)
+} uart_handler_t;
+
+/*** UART Functions ***********************************************************/
+static esp_err_t UART_Init(const uart_handler_t *uart)
+{
+	//Create a config struct
+	const uart_config_t config = {
+		.baud_rate = uart->baudrate,
+		.data_bits = UART_DATA_8_BITS,
+		.parity = UART_PARITY_DISABLE,
+		.stop_bits = UART_STOP_BITS_1,
+		.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+		.source_clk = UART_SCLK_DEFAULT,
+	};
+	
+	//Make sure RX and TX Buffer are greater than UART_HW_FIFO_LEN. TX Buffer 
+	//is allowed to be 0 if specified
+	const int min_size = UART_HW_FIFO_LEN(uart->port) + 1;
+	int rx_buff_cpy, tx_buff_cpy;
+	
+	rx_buff_cpy = (uart->rx_buff < min_size) ? min_size : uart->rx_buff;
+	tx_buff_cpy = (uart->tx_buff && uart->tx_buff < min_size) ? min_size 
+	                                                            : uart->tx_buff;
+	
+	//If the driver is already installed return OK
+	if(uart_is_driver_installed(uart->port)) return ESP_OK;
+	
+	//Init the driver & config
+	esp_err_t sta = uart_driver_install(uart->port, rx_buff_cpy, tx_buff_cpy,
+	                                                                0, NULL, 0);
+
+	if(sta == ESP_OK) sta = uart_param_config(uart->port, &config);
+	if(sta == ESP_OK) sta = uart_set_pin(uart->port, uart->tx_io, uart->rx_io, 
+	                                    UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+	
+	return sta;
+}
+
+static esp_err_t UART_Print(const uart_handler_t *uart, const char *str)
+{
+	return uart_write_bytes(uart->port, str, strlen(str));
+}
+
+static esp_err_t UART_PrintNewline(const uart_handler_t *uart, const char *str)
+{
+	esp_err_t sta = uart_write_bytes(uart->port, str, strlen(str));
+	if(sta == ESP_OK) sta = uart_write_bytes(uart->port, "\r\n", 2);
+	return sta;
+}
+
+static void UART_ReceiveString(const uart_handler_t *uart, char *str, 
+                                                               const size_t len)
+{
+	const size_t bytes_read = uart_read_bytes(uart->port, str, len - 1, 
+	                                        uart->timeout / portTICK_PERIOD_MS);	
+	//Terminate the string
+	str[bytes_read] = '\0';
+}
+
+/*** AT Commands **************************************************************/
+
+
+
